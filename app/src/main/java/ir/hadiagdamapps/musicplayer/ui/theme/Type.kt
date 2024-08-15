e8b0d9638bd4d52105e0f4f29ddd1b4616bf4533
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-16 00:20:13 +0330

    created theme

--- app/src/main/java/ir/hadiagdamapps/musicplayer/ui/theme/Type.kt
+++ app/src/main/java/ir/hadiagdamapps/musicplayer/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package ir.hadiagdamapps.musicplayer.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,12 +15,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+    ),
+    titleMedium = TextStyle(
+        color = Color.White,
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
@@ -29,6 +30,13 @@ val Typography = Typography(
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     )
-    */
+
 )
