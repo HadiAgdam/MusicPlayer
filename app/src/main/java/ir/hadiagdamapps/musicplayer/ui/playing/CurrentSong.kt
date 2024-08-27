aff04658d3cdb52b86e629e0d4e640ca49831065
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-27 14:43:14 +0330

    moved current song

---
+++ app/src/main/java/ir/hadiagdamapps/musicplayer/ui/playing/CurrentSong.kt
@@ -0,0 +1,22 @@
+package ir.hadiagdamapps.musicplayer.ui.playing
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ir.hadiagdamapps.musicplayer.models.Song
+import ir.hadiagdamapps.musicplayer.ui.theme.primary
+
+
+
+@Composable
+fun CurrentSong(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier
+            .background(primary)
+    ) {
+
+
+    }
+}
