1f4adff75e5c832a676ae5e34faf72930d41d928
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-27 15:52:18 +0330

    created MusicPlayerApp

--- app/src/main/java/ir/hadiagdamapps/musicplayer/MainActivity.kt
+++ app/src/main/java/ir/hadiagdamapps/musicplayer/MainActivity.kt
@@ -4,18 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ir.hadiagdamapps.musicplayer.navigation.Screen
+import ir.hadiagdamapps.musicplayer.ui.screens.CurrentSongScreen
 import ir.hadiagdamapps.musicplayer.ui.screens.SongScreen
+import ir.hadiagdamapps.musicplayer.ui.viewmodels.SongsViewModel
 
 class MainActivity : ComponentActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
 
-            SongScreen()
+            MusicPlayerApp()
         }
     }
 }
 
-//private val viewModel by viewModels<MusicViewModel>()
+@Composable
+fun MusicPlayerApp() {
+    val viewModel: SongsViewModel = viewModel()
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "songsScreen") {
+
+        composable(Screen.SongsScreen.route) {
+            SongScreen(navController, viewModel)
+        }
+
+        composable(Screen.CurrentSongScreen.route) {
+            CurrentSongScreen(navController, viewModel)
+        }
+
+    }
+
+}
+
+
