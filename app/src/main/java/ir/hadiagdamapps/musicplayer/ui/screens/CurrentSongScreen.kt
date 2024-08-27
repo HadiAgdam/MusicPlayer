7ab31698a5372e8d8cb0e8aac772ee5975348cf0
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-27 15:52:00 +0330

    added CurrentSongScreen

--- app/src/main/java/ir/hadiagdamapps/musicplayer/ui/screens/CurrentSongScreen.kt
+++ app/src/main/java/ir/hadiagdamapps/musicplayer/ui/screens/CurrentSongScreen.kt
@@ -1,2 +1,10 @@
 package ir.hadiagdamapps.musicplayer.ui.screens
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import ir.hadiagdamapps.musicplayer.ui.viewmodels.SongsViewModel
+
+@Composable
+fun CurrentSongScreen(navController: NavController, viewModel: SongsViewModel) {
+
+}
