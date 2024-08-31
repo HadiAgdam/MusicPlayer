4c39bbf3cfc1216efd590f03a8c998c8ea75df11
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-31 16:34:18 +0330

    created gradient light background

--- app/src/main/java/ir/hadiagdamapps/musicplayer/ui/theme/Color.kt
+++ app/src/main/java/ir/hadiagdamapps/musicplayer/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package ir.hadiagdamapps.musicplayer.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val primary = Color(0xFF09307F)
@@ -8,4 +9,9 @@ val secondary = Color(0xFF6BA6CF)
 val darkBackground = Color(0xFF151520)
 val lightBackground = Color(0xFFD6E5F4)
 
+val lightBackgroundGradient = Brush.linearGradient(
+    colors = listOf(lightBackground, Color(0xFFBCD3E8)),
+    start = androidx.compose.ui.geometry.Offset(0f, 0f),
+    end = androidx.compose.ui.geometry.Offset(1000f, 1000f)
+)
 
