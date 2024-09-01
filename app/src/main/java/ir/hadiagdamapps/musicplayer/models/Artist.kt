a8a50e44abffbb3bbc0b5733127c4218a0af4f2b
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-01 13:30:02 +0330

    deleted artist model

--- app/src/main/java/ir/hadiagdamapps/musicplayer/models/Artist.kt
+++
@@ -1,9 +0,0 @@
-package ir.hadiagdamapps.musicplayer.models
-
-class Artist private constructor(val name: String) {
-    companion object {
-        fun parse(text: String): Artist? {
-            return Artist(text)
-        }
-    }
-}
