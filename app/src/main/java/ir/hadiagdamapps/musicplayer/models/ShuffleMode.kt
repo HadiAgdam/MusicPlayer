4c70c7a95eda2bee3255dfce0f8e0472cf71ed70
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-03 14:42:33 +0330

    added more shuffle mode

--- app/src/main/java/ir/hadiagdamapps/musicplayer/models/ShuffleMode.kt
+++ app/src/main/java/ir/hadiagdamapps/musicplayer/models/ShuffleMode.kt
@@ -4,5 +4,8 @@ import androidx.annotation.DrawableRes
 import ir.hadiagdamapps.musicplayer.R
 
 enum class ShuffleMode(@DrawableRes val painterResId: Int) {
-     Repeat(R.drawable.shuffle_repeat_one_icon)
+     Repeat(R.drawable.shuffle_repeat_one_icon),
+    Shuffle(R.drawable.shuffle_repeat_icon), // TODO
+    NoRepeat(R.drawable.shuffle_random_icon), // TODO
+
 }
